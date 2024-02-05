49547a8cb4f2659b20b0c66488a7381fe623cb08
mueni <charlesmueni2@gmail.com>
2024-02-05 22:20:42 +0300

    updated EventDetails.js file

---
+++ client/src/EventDetails.js
@@ -0,0 +1,13 @@
+// EventDetails.js
+import React from 'react';
+
+const EventDetails = () => {
+  return (
+    <div>
+      <h2>Event Details</h2>
+      {/* Add code to display detailed information about the selected event */}
+    </div>
+  );
+};
+
+export default EventDetails;
